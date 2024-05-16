8736f8dff437ac0312ce33dbe248efd31aa52255
s095339 <s095339@gmail.com>
2024-05-16 17:38:29 +0800

    fix

--- avi2png.py
+++ avi2png.py
@@ -5,7 +5,7 @@ vid_name = '12_bag.avi'
 vidcap = cv2.VideoCapture(vid_name)
 success,image = vidcap.read()
 count = 0
-if not os.path.exists(vid_name): os.mkdir(vid_name[:-4])
+if not os.path.exists(vid_name[:-4]): os.mkdir(vid_name[:-4])
 print(f"frame_{count:04}")
 while success:
   cv2.imwrite(os.path.join( vid_name[:-4]  ,f"frame_{count:04}.png"), image)     # save frame as JPEG file      
