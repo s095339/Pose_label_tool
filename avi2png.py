a6c8e0cb5e324219a684f704c08aeaf9626e50ba
s095339 <s095339@gmail.com>
2024-05-16 17:35:38 +0800

    fix avi to png

--- avi2png.py
+++ avi2png.py
@@ -5,10 +5,10 @@ vid_name = '12_bag.avi'
 vidcap = cv2.VideoCapture(vid_name)
 success,image = vidcap.read()
 count = 0
-os.mkdir(vid_name[:-4])
-
+if not os.path.exists(vid_name): os.mkdir(vid_name[:-4])
+print(f"frame_{count:04}")
 while success:
-  cv2.imwrite(os.path.join( vid_name[:-4]  ,"%d.png" % count), image)     # save frame as JPEG file      
+  cv2.imwrite(os.path.join( vid_name[:-4]  ,f"frame_{count:04}.png"), image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
